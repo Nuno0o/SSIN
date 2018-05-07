e19d8652fd02aec17213665796adb3c66ce7bc21
Nuno Martins <nunojorge90@hotmail.com>
2018-05-07 17:25:32 +0100

    made voting

--- proj/contracts/Migrations.sol
+++ proj/contracts/Migrations.sol
@@ -1,23 +1,81 @@
 pragma solidity ^0.4.17;
 
-contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
-
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
-
-  function Migrations() public {
-    owner = msg.sender;
-  }
-
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
-
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
+contract Ballot {
+    address public owner;
+    // Election started
+    bool public started = false;
+    // Election ended
+    bool public ended = false;
+    // Votes
+    address[] public voters;
+    // Options to vote in
+    string[] public options;
+    // Votes
+    mapping(address => uint) private votes;
+    mapping(uint => uint) private nvotes;
+    // Only owner
+    modifier restricted() {
+        require(msg.sender == owner);
+        _;
+    }
+    modifier electionOngoing() {
+        require(started,"Voting hasn't started yet");
+        require(!ended,"Voting has ended");
+        _;
+    }
+    modifier electionEnded() {
+        require(ended,"Election hasn't ended yet");
+        _;
+    }
+    // Already voted
+    function alreadyVoted(address _a) public returns(bool){
+        bool voted = false;
+        for(uint i = 0; i < voters.length && !voted; i++){
+            voted = voters[i] == _a;
+        }
+        return voted;
+    }
+    // Option exists
+    function optionExists(uint _op) public returns(bool) {
+        return _op >= 0 && _op < options.length;
+    }
+    // Constructor
+    constructor() public {
+        owner = msg.sender;
+    }
+    // Destroy contract
+    function kill() public {
+        if(msg.sender == owner) selfdestruct(owner);
+    }
+    // Start election
+    function startVote() public restricted {
+        started = true;
+    }
+    // End election
+    function endVote() public restricted {
+        ended = true;
+    }
+    // Add option
+    function addOption(string name) public {
+        options.push(name);
+        nvotes[options.length-1] = 0;
+    }
+    // Cast vote
+    function castVote(uint _option) public electionOngoing {
+        require(!alreadyVoted(msg.sender),"You have already voted.");
+        require(optionExists(_option),"Option doesn't exist");
+        voters.push(msg.sender);
+        votes[msg.sender] = _option;
+        nvotes[_option] += 1; 
+    }
+    // Get winner
+    function getWinner() public electionEnded returns(string){
+        uint max = 0;
+        for(uint i = 0; i < options.length; i++){
+            if(nvotes[i] >= max){
+                max = i;
+            }
+        }
+        return options[max];
+    }
 }
