9af79a772ae48185de10a63c16ca722f5f35a34f
Nuno Martins <nunojorge90@hotmail.com>
2018-05-17 17:16:06 +0100

    changed contract

--- proj/ssin/src/contract/Contract.js
+++ proj/ssin/src/contract/Contract.js
@@ -1,182 +1,228 @@
-abi = [{
-    "constant": false,
-    "inputs": [{
-        "name": "nOptions",
-        "type": "uint8"
-      },
-      {
-        "name": "publicKey",
-        "type": "string"
-      }
-    ],
-    "name": "createBallot",
-    "outputs": [{
-      "name": "ballotAddress",
-      "type": "address"
-    }],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  }
+abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "nOptions",
+				"type": "uint8"
+			},
+			{
+				"name": "publicKey",
+				"type": "string"
+			}
+		],
+		"name": "createPoll",
+		"outputs": [
+			{
+				"name": "pollAddress",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "listOfPolls",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	}
 ]
 
-abi2 = [{
-    "constant": true,
-    "inputs": [],
-    "name": "getEnded",
-    "outputs": [{
-      "name": "",
-      "type": "bool"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "getNChoices",
-    "outputs": [{
-      "name": "",
-      "type": "uint256"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": false,
-    "inputs": [{
-      "name": "user",
-      "type": "address"
-    }],
-    "name": "givePermission",
-    "outputs": [{
-      "name": "",
-      "type": "bool"
-    }],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "getRightToVote",
-    "outputs": [{
-      "name": "",
-      "type": "bool"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "getChairperson",
-    "outputs": [{
-      "name": "",
-      "type": "address"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "getKey",
-    "outputs": [{
-      "name": "",
-      "type": "string"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [{
-      "name": "addr",
-      "type": "address"
-    }],
-    "name": "getVote",
-    "outputs": [{
-      "name": "",
-      "type": "string"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "constant": false,
-    "inputs": [{
-      "name": "choice",
-      "type": "string"
-    }],
-    "name": "castVote",
-    "outputs": [{
-      "name": "",
-      "type": "bool"
-    }],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "constant": false,
-    "inputs": [],
-    "name": "endVoting",
-    "outputs": [{
-      "name": "",
-      "type": "bool"
-    }],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "constant": true,
-    "inputs": [],
-    "name": "getVoters",
-    "outputs": [{
-      "name": "",
-      "type": "address[]"
-    }],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [{
-        "name": "_owner",
-        "type": "address"
-      },
-      {
-        "name": "_numProposals",
-        "type": "uint8"
-      },
-      {
-        "name": "_publicKey",
-        "type": "string"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  }
+abi2 = [
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getEnded",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getNChoices",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "user",
+				"type": "address"
+			}
+		],
+		"name": "givePermission",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getRightToVote",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getChairperson",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getKey",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "addr",
+				"type": "address"
+			}
+		],
+		"name": "getVote",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "vote",
+				"type": "string"
+			}
+		],
+		"name": "castVote",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "endVoting",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getVoters",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_owner",
+				"type": "address"
+			},
+			{
+				"name": "_numProposals",
+				"type": "uint8"
+			},
+			{
+				"name": "_publicKey",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	}
 ]
 
 
-creator = "0x903e511692f765212e8480e8f60ab8e1d4cfe8c9"
+creator = "0xcb7054f17940c41e5f0b4854ba166a2f914053cf"
 
 module.exports = {
   abi: abi,
--- proj/ssin/src/contract/Contract.sol
+++ proj/ssin/src/contract/Contract.sol
@@ -1,63 +1,109 @@
 pragma solidity ^0.4.0;
-contract Ballot {
+
+contract PollCreator {
+    // List of ongoing polls
+    Poll[] polls;
+    constructor () public {
+    }
+    function createPoll(uint8 nOptions, string publicKey) public returns (Poll pollAddress){
+        Poll p = new Poll(msg.sender, nOptions, publicKey);
+        polls.push(p);
+        return p;
+    }
+    function listOfPolls() public constant returns (Poll[]){
+        return polls;
+    }
+}
+
+contract Poll {
     struct Voter {
         bool canVote;
-        bool voted;
+        string vote;
     }
-    struct Proposal {
-        uint voteCount;
-    }
+    //Owner of the poll
     address chairperson;
+    //Id of block before creation
+    uint block_number;
+    //Map of addresses to voter
     mapping(address => Voter) voters;
-    Proposal[] proposals;
-
-    /// Create a new ballot with $(_numProposals) different proposals.
-    constructor(uint8 _numProposals) public {
-        chairperson = msg.sender;
+    //List of addresses that voted
+    address[] voted;
+    //Public key to use to encrypt votes
+    string public_key;
+    //Number of choices to vote(can't be controlled as the data is encrypted, but is used when verifying)
+    uint n_choices;
+    //Voting ended
+    bool ended;
+    /*-------------CONSTRUCTOR----------*/
+    /// Create a new poll with $(_numProposals) different proposals, public encryption key and chairperson
+    constructor(address _owner, uint8 _numProposals, string _publicKey) public {
+        chairperson = _owner;
+        n_choices = _numProposals;
+        public_key = _publicKey;
         voters[chairperson].canVote = true;
-        proposals.length = _numProposals;
+        ended = false;
+        block_number = block.number;
     }
-    /// Get address of chairperson(creator of the ballot)
+    
+    /*--------------GETTERS---------------*/
+    
+    /// Get address of chairperson(creator of the poll)
     function getChairperson() public constant returns (address) {
         return chairperson;
     }
     /// Get if user has right to vote
     function getRightToVote() public constant returns (bool) {
-        return voters[msg.sender].canVote && !voters[msg.sender].voted;
+        return voters[msg.sender].canVote;
     }
     /// Get number of proposals
-    function getNProposals() public constant returns (uint) {
-        return proposals.length;
+    function getNChoices() public constant returns (uint) {
+        return n_choices;
     }
-    /// Get number of votes on certain proposal
-    function getNVotes(uint8 index) public constant returns (uint) {
-        if(index >= proposals.length || msg.sender != chairperson) return 0;
-        return proposals[index].voteCount;
+    /// Get public encryption key
+    function getKey() public constant returns (string) {
+        return public_key;
     }
-
-    /// Give $(toVoter) the right to vote on this ballot.
-    /// May only be called by $(chairperson).
-    function giveRightToVote(address toVoter) public returns (bool) {
-        if (msg.sender != chairperson || voters[toVoter].voted) return false;
-        voters[toVoter].canVote = true;
+    /// Get if vote ended
+    function getEnded() public constant returns (bool) {
+        return ended;
+    }
+    /// Get list of voters
+    function getVoters() public constant returns (address[]){
+        return voted;
+    }
+    /// Get vote of voter
+    function getVote(address addr) public constant returns(string){
+        return voters[addr].vote;
+    }
+    
+    /*--------------TRANSACTIONS---------------*/
+    
+    /// End election
+    function endVoting() public returns (bool) {
+        ended = true;
+        return true;
+    }
+    /// Give permission to vote, can only be called by chairperson
+    function givePermission(address user) public returns (bool){
+        if(msg.sender != chairperson) return false;
+        voters[user].canVote = true;
         return true;
     }
 
-    /// Give a single vote to proposal $(toProposal).
-    function vote(uint8 toProposal) public returns (bool) {
-        Voter storage sender = voters[msg.sender];
-        if (sender.voted || toProposal >= proposals.length) return false;
-        sender.voted = true;
-        proposals[toProposal].voteCount += 1;
-        return true;
-    }
-    /// Get winning proposal
-    function winningProposal() public constant returns (uint8 _winningProposal) {
-        uint256 winningVoteCount = 0;
-        for (uint8 prop = 0; prop < proposals.length; prop++)
-            if (proposals[prop].voteCount > winningVoteCount) {
-                winningVoteCount = proposals[prop].voteCount;
-                _winningProposal = prop;
+    /// Vote
+    function castVote(string vote) public returns (bool){
+        if(voters[msg.sender].canVote != true || ended) return false;
+        voters[msg.sender].vote = vote;
+        bool alreadyVotedBefore = false;
+        for(uint i = 0; i < voted.length; i++){
+            if(voted[i] == msg.sender){
+                alreadyVotedBefore = true;
+                break;
             }
+        }
+        if(!alreadyVotedBefore){
+            voted.push(msg.sender);
+        }
+        return true;
     }
 }
