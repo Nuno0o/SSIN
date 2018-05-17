4e454c26a9855a134d312fd7118270e159d885f3
Nuno Martins <nunojorge90@hotmail.com>
2018-05-17 02:51:53 +0100

    fix path

--- proj/ssin/src/router/index.js
+++ proj/ssin/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '@/components/Main'
-import Admin from '@/components/Admin'
 
 Vue.use(Router)
 
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Main',
       component: Main
-    },
-    {
-      path: '/admin',
-      name: 'Admin',
-      component: Admin
-    },
+    }
   ]
 })
